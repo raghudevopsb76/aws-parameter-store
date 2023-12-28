d0ec0eb0f2cb4e7b33654ef965d460b355a2393f
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-12-28 06:49:51 +0530

    TF Code

--- vars.tf
+++ vars.tf
@@ -3,6 +3,8 @@ variable "parameters" {
     { name = "ses.username", type = "String", value = "AKIAZHEF5S5CFSUBQ55V" },
     { name = "dev.backend.DB_HOST", type = "String", value = "dev-mysql-rds.cmscnppwjzuf.us-east-1.rds.amazonaws.com" },
     { name = "dev.frontend.BACKEND_ENDPOINT", type = "String", value = "http://backend-dev.rdevopsb72.online/" },
+    { name = "ssh.username", type = "String", value = "root" },
+
 
     ## RDS
     { name = "dev.rds.endpoint", type = "String", value = "dev-mysql-rds.cmscnppwjzuf.us-east-1.rds.amazonaws.com" },
@@ -16,6 +18,7 @@ variable "parameters" {
     { name = "dev.rds.password", type = "SecureString", value = "ExpenseApp1234" },
     { name = "sonar.token", type = "SecureString", value = "2060dec344342f998bcb53fd3ea189592c7776ff" },
     { name = "artifactory.password", type = "SecureString", value = "Admin123" },
+    { name = "ssh.password", type = "SecureString", value = "DevOps321" },
   ]
 }
 
